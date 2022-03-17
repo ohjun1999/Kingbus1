b6f91b1fc932444f7c75c0079018c23133dbd531
ohjun99 <ohjun1999@gmail.com>
2022-03-17 14:53:27 +0900

    2022_03_17_프론트_마무리_작업

--- app/src/main/java/com/example/kingbus1/ExtraMainPassengerFragment.kt
+++ app/src/main/java/com/example/kingbus1/ExtraMainPassengerFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PersnalInfoDriverFragment.newInstance] factory method to
+ * Use the [ExtraMainPassengerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PersnalInfoDriverFragment : Fragment() {
+class ExtraMainPassengerFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class PersnalInfoDriverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.`fragment_personal_info_driver`, container, false)
+        return inflater.inflate(R.layout.fragment_extra_main_passenger, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class PersnalInfoDriverFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PersnalInfoDriverFragment.
+         * @return A new instance of fragment ExtraMainPassengerFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PersnalInfoDriverFragment().apply {
+            ExtraMainPassengerFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
